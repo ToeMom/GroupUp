ba9ddde5df513f557bbbed3b01ec9281b32e6ac8
ToeMom <miaevcmates@gmail.com>
2024-10-10 19:42:25 +0200

    Logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-
+import Image from "next/image";
+import Logo from "./img/Logo.png"
 
 export default function Home() {
   return (
     <main>
-      <p>Looooooool</p>
+      <Image src={Logo} alt="Logo" width={500} height={500}/>
     </main>
   );
 }
